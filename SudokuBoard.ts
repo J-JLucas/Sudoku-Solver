700e4ecbe07d276e55022327cfb0aa3ed7d8d1df
JoSh <jlucas205@proton.me>
2026-01-11 20:02:08 -0700

    init commit SudokuBoard class

---
+++ SudokuBoard.ts
@@ -0,0 +1,54 @@
+// union type of literal values
+// restricts CellValue to a subset of whatever values we like
+type CellValue = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | null;
+
+class SudokuBoard {
+  private grid: CellValue[][];
+
+  constructor(initial?: CellValue[][]) {
+    // copy a predefined board
+    if (initial !== undefined) {
+      if (initial.length !== 9) {
+        throw new Error("Invalid board height");
+      }
+
+      this.grid = initial.map(row => {
+        if (row.length !== 9) {
+          throw new Error("Invalid board width");
+        }
+        return row.slice();
+      });
+      return;
+    }
+
+    // construct blank board
+    this.grid = [];
+    for (let i = 0; i < 9; i++) {
+      const row: CellValue[] = [null, null, null,
+        null, null, null,
+        null, null, null];
+      this.grid.push(row);
+    }
+  }
+
+  // returns specified cell's value from the board grid
+  getCell(i: number, j: number): CellValue {
+    if (i < 0 || i >= 9 || j < 0 || j >= 9) {
+      throw new RangeError("Cell index out of bounds");
+    }
+    return this.grid[i][j];
+  }
+
+  // returns true on success, false otherwise
+  setCell(value: CellValue, i: number, j: number): boolean {
+    if (i < 0 || i >= 9 || j < 0 || j >= 9) { return false; }
+
+    this.grid[i][j] = value;
+    return true;
+  }
+
+  clone(): SudokuBoard {
+    return new SudokuBoard(this.grid);
+  }
+
+}
