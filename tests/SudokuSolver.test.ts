2d7aed3ef4b60dad99e16026f271490d230fbce7
JoSh <jlucas205@proton.me>
2026-01-12 17:41:05 -0700

    added solved board valid state test

--- tests/SudokuSolver.test.ts
+++ tests/SudokuSolver.test.ts
@@ -60,7 +60,7 @@ test("SudokuSolver: accepts a valid partially-filled board", () => {
 });
 
 
-test("SudokuSolver: NeetCode board fixture (expected true)", () => {
+test("SudokuSolver: validate NeetCode board state (expected true)", () => {
   const grid: CellValue[][] = [
     [1, 2, null, null, 3, null, null, null, null],
     [4, null, null, 5, null, null, null, null, null],
@@ -79,7 +79,7 @@ test("SudokuSolver: NeetCode board fixture (expected true)", () => {
 });
 
 
-test("SudokuSolver: NeetCode board fixture (expected false)", () => {
+test("SudokuSolver: validate NeetCode board state (expected false)", () => {
   const grid: CellValue[][] = [
     [1, 2, null, null, 3, null, null, null, null],
     [4, null, null, 5, null, null, null, null, null],
@@ -96,3 +96,42 @@ test("SudokuSolver: NeetCode board fixture (expected false)", () => {
   const s = new SudokuSolver();
   assert.equal(s.validateBoardState(b), false);
 });
+
+test("SudokuSolver: validate SudokuSpoiler.com solved board state (expected true)", () => {
+  const grid: CellValue[][] = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9],
+  ];
+
+  const b = new SudokuBoard(grid);
+  const s = new SudokuSolver();
+  assert.equal(s.validateBoardState(b), true);
+});
+
+
+/*
+test("SudokuSolver: solve  board (expected true)", () => {
+  const grid: CellValue[][] = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9],
+  ];
+
+  const b = new SudokuBoard(grid);
+  const s = new SudokuSolver();
+  assert.equal(s.validateBoardState(b), true);
+});
+*/
